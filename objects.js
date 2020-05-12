8753b27ecc41e04154dc5570b0599b5a04aebd05
MatveevaLI <ladaivanna.matveeva@gmail.com>
2020-05-12 15:39:13 +0200

    move

--- objects.js
+++ objects.js
@@ -1,51 +1,125 @@
-
 class Pacman extends BaseObject {
     // Initialization
-    constructor() {
+    constructor(currentLevel) {
         super(150, 250, 20, 20);
         this.canvas = document.getElementById("canvas");
         this.image = resourceManager.getImageSource('pacman');
-        
         this.x = 150;
         this.y = 250;
         this.height = 20;
         this.width = 20;
+        this.currentLevel = currentLevel;
     }
+
+    getCoordinate(x, y) {
+        if (this.currentLevel && this.currentLevel[x] && this.currentLevel[y]) {
+            return this.currentLevel[y][x];
+        }
+    }
+
+    handleMovement(pacman, newCoordinate, currentCoordinate, offset) {
+        if (offset == "+X") {
+            pacman.x++;
+        }
+        else if (offset == "-X") {
+            pacman.x--;
+        }
+        else if (offset == "+Y") {
+            pacman.y++;
+        }
+        else if (offset == "-Y") {
+            pacman.y--;
+        }
+        // const dxMin = Math.floor(pacman.x / 20) * 20;
+
+        // const dyMin = Math.floor(pacman.y / 20) * 20;
+        // const dyMax = Math.ceil(pacman.y / 20) * 20;
+
+        // if (newCoordinate.x * 20 < dxMin || newCoordinate.x * 20 > dxMax) {
+        //     pacman.x = newCoordinate.x * 20;
+        // }
+        // if (newCoordinate.y * 20 < dyMin || newCoordinate.y * 20 > dyMax) {
+        //     pacman.y = newCoordinate.y * 20;
+        // }
+    }
+
     // Movement logic
     move(dt) {
-      var dx = 5;
-      var dy = 5;
-          if (keys[37]) {
-              if (this.x - dx <= 10)    
-                  this.x = 8;
-              this.x = this.x - dx;
-          }
-          if (keys[39]) {    
-              if (this.x > 460)  
-                  this.x = this.x - dx;
-              this.x = this.x + dx ;
-          }
-          if (keys[38]) {
-              if (this.y + dy <=10 ) 
-                  this.y =this.y + dy;
-              this.y = this.y - dy ;
-          }
-          if (keys[40]) {
-              if (this.y + dy >=470 )
-                  this.y = this.y - dy;
-              this.y = this.y + dy;
-          };
-  
+        
+        let currentCoordinate = this.getCoordinate(Math.floor((this.x) / 20), Math.floor(this.y / 20));
+        let newCoordinate = null;
+        let offset = "";
+        
+        let currentPointX = Math.floor(this.x / 20);
+        let currentPointY = Math.floor(this.y / 20);
+
+        if (keys["ArrowLeft"]) {
+            let newPointX = Math.floor((this.x - 1) / 20);
+            if (newPointX == currentPointX) {
+                this.x--;
+                return;
+            }
+            else {
+                newCoordinate = this.getCoordinate(newPointX, currentPointY);
+                offset = "-X";
+            }
+        }
+
+        if (keys["ArrowRight"]) {
+            
+            let newPointX = Math.floor((this.x + 1) / 20);
+            if (newPointX == currentPointX) {
+                this.x++;
+                return;
+            }
+            else {
+                newCoordinate = this.getCoordinate(newPointX, currentPointY);
+                offset = "+X";
+            }
+        }
+
+        if (keys["ArrowUp"]) {
+            let newPointY = Math.floor((this.y - 1) / 20);
+            if (newPointY == currentPointY) {
+                this.y--;
+                return;
+            }
+            else {
+                newCoordinate = this.getCoordinate(currentPointX, newPointY);
+                offset = "-Y";
+            }
+        }
+        if (keys["ArrowDown"]) {
+            let newPointY = Math.floor((this.y + 1) / 20);
+            if (newPointY == currentPointY) {
+                this.y++;
+                return;
+            }
+            else {
+                newCoordinate = this.getCoordinate(currentPointX, newPointY);
+                offset = "+Y";
+            }
+        };
+        if (newCoordinate) {
+            
+            switch (newCoordinate.type) {
+                case Cell.CellType.Empty: this.handleMovement(this, newCoordinate, currentCoordinate, offset);
+                case Cell.CellType.Wall: break;
+                case Cell.CellType.Food: break;
+                case Cell.CellType.BigFood: break;
+                default: throw "Error";
+            }
+        }
     }
     update(dt) {
         this.move(dt);
-      }
-  
+    }
+
     // Render self
     render(ctx) {
-      ctx.save();
-      ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
-      ctx.restore();
+        ctx.save();
+        ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
+        ctx.restore();
     }
 }
 
@@ -54,7 +128,7 @@ class Duch extends BaseObject {
         super(Math.random() * canvas.width, Math.random() * 490, 40, 40);
         this.canvas = document.getElementById("canvas");
         this.image = resourceManager.getImageSource('duch');
-  
+
         this.x = Math.random() * canvas.width;
         this.y = Math.random() * 490;
         this.dx = Math.random() * 50 - 25;
@@ -64,33 +138,33 @@ class Duch extends BaseObject {
     }
 
     handleEvent(ev) {
-        
+
     }
-  
-  // Movement logic
+
+    // Movement logic
     move(dt) {
-          const canvas = this.canvas;
-          if (this.x > canvas.width) {
-          this.x = canvas.width
-          this.dx = -Math.abs(this.dx)
-          }
-          if (this.x < 0) {
-              this.x = 0
-              this.dx = Math.abs(this.dx)
-          }
-          if (this.y > canvas.height) {
-              this.y = 490
-              this.dy = -Math.abs(this.dy)
-          }
-          if (this.y < 0) {
-              this.y = 0
-              this.dy = Math.abs(this.dy) * 0.2
-          }
-  
-          // Movement
-          this.x += this.dx * dt
-          this.y += 0 * dt
-          this.rotation +=dt/3
+        const canvas = this.canvas;
+        if (this.x > canvas.width) {
+            this.x = canvas.width
+            this.dx = -Math.abs(this.dx)
+        }
+        if (this.x < 0) {
+            this.x = 0
+            this.dx = Math.abs(this.dx)
+        }
+        if (this.y > canvas.height) {
+            this.y = 490
+            this.dy = -Math.abs(this.dy)
+        }
+        if (this.y < 0) {
+            this.y = 0
+            this.dy = Math.abs(this.dy) * 0.2
+        }
+
+        // Movement
+        this.x += this.dx * dt
+        this.y += 0 * dt
+        this.rotation += dt / 3
     }
     update(dt) {
         this.move(dt);
@@ -104,12 +178,12 @@ class Duch extends BaseObject {
         ctx.restore()
     }
 }
- 
+
 class Food {
     constructor(x, y, width, height) {
         this.canvas = document.getElementById("canvas");
         this.image = resourceManager.getImageSource('food');
-    
+
         this.x = x;
         this.y = y;
         this.width = width;
@@ -123,35 +197,28 @@ class Food {
     }
 }
 
-class Sound
-{
-    constructor(src)
-    {
-        this.sound = resourceManager.getSoundSource(src);   
+class Sound {
+    constructor(src) {
+        this.sound = resourceManager.getSoundSource(src);
         this.isPlaying = false;
     }
 
-    play()
-    {
+    play() {
         this.sound.play();
         this.sound.muted = false;
     }
 
-    pause()
-    {
+    pause() {
         // this.sound.pause();
         this.sound.muted = true;
     }
 
-    playsound()
-    {
-        if(this.isPlaying == false)
-        {
+    playsound() {
+        if (this.isPlaying == false) {
             this.play();
             this.isPlaying = true;
         }
-        else 
-        {
+        else {
             this.pause();
             this.isPlaying = false;
         }
